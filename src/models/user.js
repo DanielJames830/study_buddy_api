0679ed88235b02a0138098c312d783680b9d416f
DanielJames830 <danielmanley830@gmail.com>
2024-02-07 10:44:41 -0500

    sendgrid

--- src/models/user.js
+++ src/models/user.js
@@ -1,38 +1,69 @@
-const mongoose = require('mongoose') 
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const validator = require("validator")
+const bcrypt = require("bcrypt")
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({ 
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-      lowercase: true
-    },
-    username: { 
-        type: String,
-        required: true, 
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 8
-      },
-    school: {
-        type: String,
-        required: true
-    },
-    email_verified: {
-        type: Boolean,
-        default: false
-    },
-    majors: [String],
-    tokens: [String],
-    profile_pic: Buffer
+const userSchema = new Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error("Email is invalid.");
+			}
+		},
+	},
+	username: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minLength: 8,
+	},
+	school: {
+		type: String,
+		required: true,
+	},
+	email_verified: {
+		type: Boolean,
+		default: false,
+	},
+	majors: [String],
+	tokens: [String],
+	profile_pic: Buffer,
+});
+
+userSchema.pre('save', async function(next) {
+  
+  const user = this
+  
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  
+  next()  // run the save() method
 })
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.toJSON = function () {
+	const user = this;
 
-module.exports = User
+	const userObject = user.toObject();
+
+	delete userObject.password;
+	delete userObject.tokens;
+	delete userObject.email_verified;
+	delete userObject.__v;
+
+	return userObject;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
